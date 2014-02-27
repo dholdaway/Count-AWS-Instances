d166dddb88b1753be35c4dcdab29db504cb5151a
dholdaway <d.m.holdaway@googlemail.com>
2014-02-27 17:39:20 +0000

    added get account info

---
+++ get_account.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+clear
+echo "Getting Profiles"
+grep profile ~/.aws/config | sed 's/\[//g;s/\]//g' | sed "s/\profile\b/ /g" > /tmp/profiles.txt
+grep Account ~/.aws/config | sed 's/\[//g;s/\]//g' | sed "s/\Account\b/ /g" > /tmp/accounts.txt
+grep Name ~/.aws/config | sed 's/\[//g;s/\]//g' | sed "s/\Name\b/ /g"  > /tmp/names.txt
+pr -m -t -s\  /tmp/accounts.txt /tmp/profiles.txt /tmp/names.txt | awk '{print $3,$4,$5,","$6,$1,",",$2}' > /tmp/account_profile.csv
+cat /tmp/account_profile.csv
+exit 0
+#"s/^some\s//g"
+#sed -e 's/^\w*\ *//' test.txt
