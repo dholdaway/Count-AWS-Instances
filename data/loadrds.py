1aa857b42b45903ae25c983a61e621094c4d548a
dholdaway <d.m.holdaway@googlemail.com>
2014-03-03 15:08:10 +0000

    final

--- data/loadrds.py
+++ data/loadrds.py
@@ -8,6 +8,8 @@ mydb = MySQLdb.connect(host='localhost',
     passwd='3upHEf2c',
     db='aws')
 cursor = mydb.cursor()
+#	cursor.execute('alter table total modify rds set default 0')
+#mydb.commit()
 csv_data = csv.reader(file('/tmp/daily_count_rds.csv'))
 for row in csv_data:
 	cursor.execute('UPDATE total SET rds = COALESCE (rds, 0) + %s where aws_profile_name = "%s"',row)
